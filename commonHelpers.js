ff631e29c11647b12184e162116e33ed05f536a6
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-07-13 13:24:25 +0000

    Deploying to gh-pages from @ bartoszkucharuk/goit-js-hw-11@3ec66968ccdb6158cda8e4b3132c9f64cd5f341f 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,26 +1,29 @@
-import{S as h,i as m}from"./assets/vendor-8c59ed88.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))c(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&c(i)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function c(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const p="https://pixabay.com/api/",y="44872402-0762bbf0a5ccd686fb6258473",g="photo",L="horizontal",$=!0;function b(s){return fetch(`${p}?key=${y}&q=${s}&image_type=${g}&orientation=${L}&safesearch=${$}`).then(r=>{if(!r.ok)throw new Error(r.status);return r.json()})}function S({hits:s},r){if(s.length===0){n();return}const o=s.map(({webformatURL:e,largeImageURL:t,tags:i,likes:l,views:u,comments:d,downloads:f})=>`
-            <li class="item">
+import{S as f,i as h}from"./assets/vendor-8c59ed88.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const c of t.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&s(c)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const m="https://pixabay.com/api/",y="44872402-0762bbf0a5ccd686fb6258473",g="photo",L="horizontal",$=!0;function b(i){return fetch(`${m}?key=${y}&q=${i}&image_type=${g}&orientation=${L}&safesearch=${$}`).then(r=>{if(!r.ok)throw new Error(r.status);return r.json()})}function S({hits:i},r){if(i.length===0){a();return}const o=i.map(({webformatURL:e,largeImageURL:t,tags:c,likes:l,views:d,comments:u,downloads:p})=>`
+            <li class="list-item">
                 <a href="${t}">
-                    <img class="image" src="${e}" alt="${i}" width="360">
+                    <img class="card-image"
+                    src="${e}"
+                    alt="${c}"
+                    width="360">
                 </a>
-                <ul class="descr-list">
-                    <li class="descr-item">
-                        <h3 class="descr-title">Likes</h3>
-                        <p class="descr-value">${l}</p>
+                <ul class="card-description-list">
+                    <li class="card-description-item">
+                        <h3 class="card-description-title">Likes</h3>
+                        <p class="card-description-value">${l}</p>
                     </li>
-                    <li class="descr-item">
-                        <h3 class="descr-title">Views</h3>
-                        <p class="descr-value">${u}</p>
+                    <li class="card-description-item">
+                        <h3 class="card-description-title">Views</h3>
+                        <p class="card-description-value">${d}</p>
                     </li>
-                    <li class="descr-item">
-                        <h3 class="descr-title">Comments</h3>
-                        <p class="descr-value">${d}</p>
+                    <li class="card-description-item">
+                        <h3 class="card-description-title">Comments</h3>
+                        <p class="card-description-value">${u}</p>
                     </li>
-                    <li class="descr-item">
-                        <h3 class="descr-title">Downloads</h3>
-                        <p class="descr-value">${f}</p>
+                    <li class="card-description-item">
+                        <h3 class="card-description-title">Downloads</h3>
+                        <p class="card-description-value">${p}</p>
                     </li>
                 </ul>
             </li>
-        `).join("");r.innerHTML=o,new h(".card-container a",{captions:!0,captionsData:"alt",captionPosition:"bottom",captionDelay:250}).refresh()}function n(){m.error({message:"Sorry, there are no images matching your search query. Please try again!"})}const w=document.querySelector(".form-search"),v=document.querySelector(".card-container"),a=document.querySelector(".loader");w.addEventListener("submit",P);function P(s){s.preventDefault();const r=s.currentTarget,o=r.elements.search.value.toLowerCase();q(),b(o).then(c=>S(c,v)).catch(n).finally(()=>{r.reset(),E()})}function q(){a.style.display="flex"}function E(){a.style.display="none"}
+        `).join("");r.innerHTML=o,new f(".card-container a",{captions:!0,captionsData:"alt",captionPosition:"bottom",captionDelay:250}).refresh()}function a(){h.error({message:"Sorry, there are no images matching your search query. Please try again!"})}const w=document.querySelector(".form-search"),v=document.querySelector(".card-container"),n=document.querySelector(".loader");w.addEventListener("submit",P);function P(i){i.preventDefault();const r=i.currentTarget,o=r.elements.search.value.toLowerCase();q(),b(o).then(s=>S(s,v)).catch(a).finally(()=>{r.reset(),E()})}function q(){n.style.display="flex"}function E(){n.style.display="none"}
 //# sourceMappingURL=commonHelpers.js.map
